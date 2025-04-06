bd746f2e1ecab0bc2c2151328991b58eba787e8c
Haris Dautović <haris.dautovic.dev@gmail.com>
2025-04-06 16:11:36 +0200

    Bridge: Init buffer-profiler-bridge module

---
+++ buffer-profiler-bridge/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    `java-library`
+}
+
+group = "com.harisdautovic"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+// Configure Java Library components
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+// Configure the existing configurations to include Kotlin platform type
+configurations {
+    named("apiElements") {
+        attributes {
+            attribute(org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.attribute, org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.jvm)
+        }
+    }
+
+    named("runtimeElements") {
+        attributes {
+            attribute(org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.attribute, org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.jvm)
+        }
+    }
+}
